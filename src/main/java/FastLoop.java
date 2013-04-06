f7d33794219f5a885458a486bfe7805e488080dd
Rob Austin <robaustin123@googlemail.com>
2013-04-06 23:25:30 +0100

    initial version

---
+++ src/main/java/FastLoop.java
@@ -0,0 +1,56 @@
+/**
+ * most cpu's have the ability to compare zero and jump as a single operation
+ */
+public class FastLoop {
+
+    public static long work = 1;
+
+    public static long unwoundLoop(long times) {
+
+
+        final long l = times / 2;
+
+        long start = System.nanoTime();
+
+        for (long i = 0; i < l; i++) {
+            doWork();
+            doWork();
+
+        }
+
+        return System.nanoTime() - start;
+
+    }
+
+    public static long forLoop(long times) {
+
+        long start = System.nanoTime();
+
+        for (long i = 0; i < times; i++) {
+            doWork();
+        }
+
+        return System.nanoTime() - start;
+
+    }
+
+    public static void main(String... args) {
+
+
+        for (int pwr = 2; pwr < 11; pwr++) {
+            long i = (long) Math.pow(10, pwr);
+            long time1 = unwoundLoop(i);
+            long time2 = forLoop(i);
+            System.out.printf("Performing %,d loops, unwoundLoop() took %.3f us and forLoop() took %.3f us on average, ratio=%.1f%n",
+                    i, time1 / 1e3, time2 / 1e3, (double) time1 / time2);
+        }
+
+        System.out.println("\nJust printing work so that it is not optimized out, work=" + work);
+
+    }
+
+    public static long doWork() {
+        work += 1;
+        return work;
+    }
+}
