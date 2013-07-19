a24ec18c48eb3687a66cca21c2180c6b5b948e81
Rob Austin <robaustin123@googlemail.com>
2013-07-19 11:33:14 +0100

    initial version

--- src/main/java/LazySet.java
+++ src/main/java/LazySet.java
@@ -4,7 +4,6 @@ public class LazySet {
 
 
     private static final long valueOffset;
-
     static long offset;
     static sun.misc.Unsafe unsafe;
     volatile Object o;
@@ -21,12 +20,57 @@ public class LazySet {
         }
     }
 
+    public static void main(String... args) throws NoSuchFieldException {
+
+        final LazySet that = new LazySet();
+
+        for (int pwr = 2; pwr < 11; pwr++) {
+            long i = (long) Math.pow(10, pwr);
+            long time1 = that.callPutOrderedObject(i);
+            long time2 = that.setTheVolatileDirectly(i);
+            System.out.printf("Performing %,d loops, callPutOrderedObject() took %.3f us and setting the volatile directly took %.3f us on average, ratio=%.1f%n",
+                    i, time1 / 1e3, time2 / 1e3, (double) time1 / time2);
+        }
+
+        System.out.println("\nJust printing work so that it is not optimized out, work=" + that.o);
+
+    }
+
     /**
      * @param o It is only really useful where the field is volatile, and is thus expected to change unexpectedly.
      * @throws NoSuchFieldException
      */
     public void lazySet(Object o) throws NoSuchFieldException {
         unsafe.putOrderedObject(this, valueOffset, o);
+    }
+
+    public long callPutOrderedObject(long times) throws NoSuchFieldException {
+
+
+        final long l = times / 2;
+
+        long start = System.nanoTime();
+
+        for (long i = 0; i < l; i++) {
+            lazySet("do work");
+        }
+
+        return System.nanoTime() - start;
+
+    }
+
+    public long setTheVolatileDirectly(long times) {
+
+
+        final long l = times / 2;
+
+        long start = System.nanoTime();
+
+        for (long i = 0; i < l; i++) {
+            o = "do work";
+        }
+
+        return System.nanoTime() - start;
 
     }
 
